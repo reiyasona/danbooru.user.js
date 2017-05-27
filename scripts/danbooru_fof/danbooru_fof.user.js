945d6fbe1ff977053d9ac3eb16d276c5f26772e8
reiyasona <reiyasona@users.noreply.github.com>
2017-05-28 00:22:25 +0200

    Don't fade out favorites for queries containing "fav:<ownusername>"
    Additional changes:
    - Add primitive logging
    - Refactor

--- scripts/danbooru_fof/danbooru_fof.user.js
+++ scripts/danbooru_fof/danbooru_fof.user.js
@@ -6,48 +6,61 @@
 // @include      http*://*.donmai.us/posts*
 // @include      http*://*.donmai.us/explore*
 // @include      http*://*.donmai.us/
-// @version      2017-05-26T15:59:17Z
+// @version      2017-05-27T21:40:55Z
 // @grant        none
 // @run-at       document-idle
 // ==/UserScript==
 
-var userId = $('meta[name="current-user-id"]').attr("content");
+var logging = true;
 
-var postIds = $(".post-preview").map((i, post) => $(post).data("id")).toArray();
+var userName = $("meta[name='current-user-name']").attr("content");
+if (logging) {console.log("userName:", userName)};
 
-$.getJSON(`/posts.json?tags=id:${postIds.join(",")}`).done(
-		function (json) {
-			var favedPosts = getObjectsIndexOf(json, 'fav_string', 'fav:' + userId);
-			var favedIds = getValues(favedPosts, 'id');
-			fadePosts(favedIds);
-		}
+var userId = $("meta[name='current-user-id']").attr("content");
+if (logging) {console.log("userId:", userId)};
 
-);
+if (document.title.indexOf("fav:" + userName) === -1) {
+
+	var postIds = $(".post-preview").map((i, post) => $(post).data("id")).toArray();
+	if (logging) {console.log("postIds:", postIds)};
+
+	$.getJSON(`/posts.json?tags=id:${postIds.join(",")}`).done(
+			function (json) {
+				if (logging) {console.log("json:", json)};
+				var favedPosts = getObjects(json, "fav_string", "fav:" + userId);
+				if (logging) {console.log("favedPosts:", favedPosts)};
+				var favedIds = getValues(favedPosts, "id");
+				if (logging) {console.log("favedIds:", favedIds)};
+				fadePosts(favedIds);
+			}
+	);
+};
 
 function fadePosts(favedIds) {
 	var i, len;
 	for (var i = 0, len = favedIds.length; i < len; i++) {
-		$('#post_' + favedIds[i]).fadeTo("slow", 0.15);
-		$('document').ready(function() {
-			$('#post_' + favedIds[i]).hover(function() {
+		$("#post_" + favedIds[i]).fadeTo("slow", 0.15);
+		$("document").ready(function() {
+			$("#post_" + favedIds[i]).hover(function() {
 				$(this).fadeTo(0, 1);
 				}, function() {
 				$(this).fadeTo(0, 0.15);
 			});
 		});
+		if (logging) {console.log("favedIds[" + i + "]:", favedIds[i])};
 	}
 }
 
-function getObjectsIndexOf(obj, key, val) {
+function getObjects(obj, key, val) {
 	var objects = [];
 	for (var i in obj) {
 		if (!obj.hasOwnProperty(i)) continue;
-		if (typeof obj[i] == 'object') {
-			objects = objects.concat(getObjectsIndexOf(obj[i], key, val));
+		if (typeof obj[i] == "object") {
+			objects = objects.concat(getObjects(obj[i], key, val));
 		} else
-			if (i == key && obj[i].indexOf(val) !== -1 || i == key && val == '') {
+			if (i == key && obj[i].indexOf(val) !== -1 || i == key && val == "") {
 				objects.push(obj);
-			} else if (obj[i] == val && key == ''){
+			} else if (obj[i] == val && key == ""){
 				if (objects.lastIndexOf(obj) == -1){
 					objects.push(obj);
 				}
@@ -60,7 +73,7 @@ function getValues(obj, key) {
 	var objects = [];
 	for (var i in obj) {
 		if (!obj.hasOwnProperty(i)) continue;
-		if (typeof obj[i] == 'object') {
+		if (typeof obj[i] == "object") {
 			objects = objects.concat(getValues(obj[i], key));
 		} else if (i == key) {
 			objects.push(obj[i]);
