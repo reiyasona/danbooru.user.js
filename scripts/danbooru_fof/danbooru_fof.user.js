01ffa5d890d58e8df9f74679260fe27858bd7f7e
reiyasona <reiyasona@users.noreply.github.com>
2021-01-07 02:34:17 +0000

    Fixed JSON query for "Posts per page" setting >20
    Additional changes:
    - Move functionality from collectFavedPostsJson() function
    - Remove unnecessary collectFavedPostsJson() function
    - Various refactorings

--- scripts/danbooru_fof/danbooru_fof.user.js
+++ scripts/danbooru_fof/danbooru_fof.user.js
@@ -6,7 +6,7 @@
 // @include      http*://*.donmai.us/posts*
 // @include      http*://*.donmai.us/explore*
 // @include      http*://*.donmai.us/
-// @version      2021-01-06T06:08:36Z
+// @version      2021-01-07T03:16:00Z
 // @grant        none
 // @run-at       document-idle
 // ==/UserScript==
@@ -15,56 +15,31 @@ var userId = $("body").attr("data-current-user-id");
 console.log("userId:", userId);
 
 if (userId !== "null") {
-	
 	var userName = $("body").attr("data-current-user-name").toLowerCase();
 	console.log("userName:", userName);
 	
 	if (document.title.toLowerCase().indexOf("fav:" + userName) === -1) {
-		
 		var postIds = $(".post-preview").map((i, post) => $(post).data("id")).toArray();
 		console.log("postIds:", postIds);
 		
-		var favedPostIds = [];
+		var postIdsCount = postIds.length;
+		console.log("postIdsCount:", postIdsCount);
 		
-		var useHtml = false;
-		console.log("useHtml:", useHtml);
-		
-		switch (useHtml) {
-			case true:
-			console.log("Using HTML mode! [DEPRECATED: https://github.com/danbooru/danbooru/issues/4652#issuecomment-754993802]");
-			collectFavedPostsHtml();
-			fadePosts();
-			break;
-			default:
-			console.log("Using JSON mode!");
-			collectFavedPostsJson().then(function(){ fadePosts(); });
+		for (var i = 0, len = postIdsCount; i < len; i += 20) {
+			$.getJSON(`/favorites.json?search[post_id]=${postIds.slice(i, i + 20).join(",")}&search[user_id]=${userId}`).done(
+				function (favedPostsJson) {
+					if (favedPostsJson.length > 0) { console.log("favedPostsJson:", favedPostsJson); }
+					
+					favedPostIds = getValues(favedPostsJson, "post_id");
+					if (favedPostIds.length > 0) { console.log("favedPostIds:", favedPostIds); }
+					
+					fadePosts(favedPostIds);
+				}
+			);
 		}
 	}
 } else {
-	console.log("The User ID is null! User not logged in?");
-}
-
-function collectFavedPostsHtml() {
-	var postFavStatuses = $(".post-preview").map((i, postFavStatuses) => $(postFavStatuses).data("is-favorited")).toArray();
-	console.log("postFavStatuses:", postFavStatuses);
-	
-	for (var i = 0, len = postIds.length; i < len; i++) {
-		if (postFavStatuses[i]) {
-			favedPostIds.push(postIds[i]);
-		}
-	}
-	console.log("favedPostIds:", favedPostIds);
-}
-
-function collectFavedPostsJson() {
-	return $.getJSON(`/favorites.json?search[post_id]=${postIds.join(",")}&search[user_id]=${userId}`).done(
-		function (favedPostsJson) {
-			console.log("favedPostsJson:", favedPostsJson);
-			
-			favedPostIds = getValues(favedPostsJson, "post_id");
-			console.log("favedPostIds:", favedPostIds);
-		}
-	);
+	console.log("The user ID is null! User not logged in?");
 }
 
 function fadePosts() {
@@ -80,14 +55,14 @@ function fadePosts() {
 }
 
 function getValues(obj, key) {
-	var objects = [];
+	var values = [];
 	for (var i in obj) {
 		if (!obj.hasOwnProperty(i)) continue;
 		if (typeof obj[i] == "object") {
-			objects = objects.concat(getValues(obj[i], key));
+			values = values.concat(getValues(obj[i], key));
 		} else if (i == key) {
-			objects.push(obj[i]);
+			values.push(obj[i]);
 		}
 	}
-	return objects;
+	return values;
 }
